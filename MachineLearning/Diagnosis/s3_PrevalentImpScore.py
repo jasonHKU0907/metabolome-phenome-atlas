9e032d3d2850981af76acecdc58ebfa0c4b0b539
JiaYou@FudanUniv <56468872+jasonHKU0907@users.noreply.github.com>
2025-02-17 10:42:18 +0800

    Add files via upload

---
+++ MachineLearning/Diagnosis/s3_PrevalentImpScore.py
@@ -0,0 +1,60 @@
+
+import glob
+import re
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import warnings
+import matplotlib.pyplot as plt
+from scipy import interp
+from sklearn.metrics import roc_curve
+import pandas as pd
+import seaborn as sns
+warnings.filterwarnings('error')
+
+def sort_nicely(l):
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [ convert(c.replace("_","")) for c in re.split('([0-9]+)', key) ]
+    l.sort( key=alphanum_key )
+    return l
+
+dpath = '/Volumes/JasonWork/Projects/MetaAtlas/'
+tgt_dir_lst = sort_nicely(glob.glob(dpath + 'Revision/Results/Prediction/Prevalent/NMR_Importance/*.csv'))
+tgt_dict = pd.read_csv(dpath + 'Data/TargetData/DiseaseTable.csv', encoding='latin-1')
+met_dict = pd.read_csv(dpath + 'Data/MetaData/name_anno.csv')
+
+
+for tgt_dir in tqdm(tgt_dir_lst):
+    tgt = os.path.basename(tgt_dir)[:-4]
+    tgt_name = tgt_dict.loc[tgt_dict.NAME == tgt].LONGNAME.iloc[0]
+    output_img = dpath + 'Revision/Results/Prediction/Prevalent/Importance_Plot/' + tgt + '.pdf'
+    tgt_pred_df = pd.read_csv(tgt_dir)
+    tgt_pred_df = tgt_pred_df.iloc[:30,:]
+    tgt_pred_df = pd.merge(tgt_pred_df, met_dict, how = 'left', left_on = 'NMR_code', right_on='Metabolite_code')
+    fig, ax = plt.subplots(figsize=(12, 6))
+    plt.rcParams["font.family"] = "Arial"
+    palette = sns.color_palette("Blues", n_colors=len(tgt_pred_df))
+    palette.reverse()
+    sns.barplot(ax=ax, x="plot_code", y="Importance", palette=palette, data=tgt_pred_df.sort_values(by="Importance", ascending=False))
+    y_imp_up_lim = round(tgt_pred_df['Importance'].max() + 0.005, 2)
+    ax.set_ylim([0, y_imp_up_lim])
+    ax.tick_params(axis='y', labelsize=16)
+    ax.set_xticklabels(tgt_pred_df['plot_code'], rotation=45, fontsize=14, horizontalalignment='right')
+    ax.set_ylabel('Normalized metabolite importance', weight='bold', fontsize=14)
+    # ax.set_title(my_title, y=1.0, pad=-25, weight='bold', fontsize=24)
+    ax.set_xlabel('')
+    ax.grid(which='minor', alpha=0.2, linestyle=':')
+    ax.grid(which='major', alpha=0.5, linestyle='--')
+    ax.set_axisbelow(True)
+    if len(tgt_name)<45:
+        plt.title(tgt_name, fontsize = 32, weight='bold', family='Arial')
+    elif (len(tgt_name)>=45)&(len(tgt_name)<60):
+        plt.title(tgt_name, fontsize = 24, weight='bold', family='Arial')
+    else:
+        plt.title(tgt_name, fontsize = 16, weight='bold', family='Arial')
+    plt.tight_layout()
+    plt.savefig(output_img)
+    plt.close('all')
+
+
---
+++ MachineLearning/ImportanceSummaryGenerator/TopRank_Summary.py
@@ -0,0 +1,53 @@
+
+import glob
+import re
+import os
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import warnings
+import matplotlib.pyplot as plt
+from scipy import interp
+from sklearn.metrics import roc_curve
+import pandas as pd
+import seaborn as sns
+
+def sort_nicely(l):
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [ convert(c.replace("_","")) for c in re.split('([0-9]+)', key) ]
+    l.sort( key=alphanum_key )
+    return l
+
+type = 'Incident'
+type = 'Prevalent'
+
+top_nb = 1
+dpath = '/Volumes/JasonWork/Projects/MetaAtlas/'
+tgt_dir_lst = sort_nicely(glob.glob(dpath + 'Revision/Results/Prediction/'+type+'/NMR_Importance/*.csv'))
+tgt_dict = pd.read_csv(dpath + 'Data/TargetData/DiseaseTable.csv', encoding='latin-1')
+tgt_dict = tgt_dict.loc[tgt_dict[type+'_analysis'] == 1]
+root_lst = list(set(tgt_dict.Root.tolist()))
+root_lst.sort()
+nmr_code_df = pd.read_csv(tgt_dir_lst[0], usecols = ['NMR_code'])
+nmr_code_df.sort_values(by = 'NMR_code', inplace = True)
+root_imp_df = nmr_code_df.copy()
+
+
+for root in tqdm(root_lst):
+    root_name = root.split(' ')[0] + '_' + root.split(' ')[1]
+    name_lst = tgt_dict.loc[tgt_dict.Root == root].NAME.tolist()
+    name_imp_df = nmr_code_df.copy()
+    for name in name_lst:
+        tgt_dir = dpath + 'Revision/Results/Prediction/'+type+'/NMR_Importance/'+name+'.csv'
+        tgt_imp_df = pd.read_csv(tgt_dir, usecols = ['NMR_code', 'Importance'])
+        tgt_imp_df.sort_values(by = 'Importance', ascending=False, inplace = True)
+        tgt_imp_df[name] = [1]*top_nb + [0]*(len(tgt_imp_df)-top_nb)
+        name_imp_df = pd.merge(name_imp_df, tgt_imp_df[['NMR_code', name]], how = 'left', on = ['NMR_code'])
+    name_imp_df[root_name] = name_imp_df.iloc[:, 1:].sum(axis=1).tolist()
+    name_imp_df.to_csv(dpath + 'Revision/Results/Prediction/'+type+'/ImportanceSummary/Top'+str(top_nb)+'_summary/'+root_name+'.csv', index = False)
+    root_imp_df = pd.merge(root_imp_df, name_imp_df[['NMR_code', root_name]], how = 'left', on = ['NMR_code'])
+
+root_imp_df['Total'] = root_imp_df.iloc[:, 1:].sum(axis=1).tolist()
+root_imp_df.to_csv(dpath + 'Revision/Results/Prediction/'+type+'/ImportanceSummary/Top'+str(top_nb)+'_summary/Importance_by_root.csv', index = False)
+
+
